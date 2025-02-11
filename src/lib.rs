8896075006688db4d0a3124500b3be2393f6b6cf
lazymio <mio@lazym.io>
2025-02-11 21:24:13 +0800

    Allow custom model

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use derive_more::derive::Display;
 use serde::{Deserialize, Serialize};
 
 // General models, note might alias to a specific model
-#[derive(Debug, Clone, Serialize, Deserialize, Display, Copy)]
+#[derive(Debug, Clone, Serialize, Deserialize, Display)]
 pub enum OpenAIModel {
     #[display("gpt-4o")]
     GPT4O,
@@ -19,7 +19,9 @@ pub enum OpenAIModel {
     #[display("gpt-4")]
     GPT4,
     #[display("gpt-4-turbo")]
-    GPT4TURBO
+    GPT4TURBO,
+    #[display("{_0}")]
+    Other(String, PricingInfo),
 }
 
 impl FromStr for OpenAIModel {
@@ -33,14 +35,14 @@ impl FromStr for OpenAIModel {
             "o1" => Ok(Self::O1),
             "o1-mini" => Ok(Self::O1MINI),
             "gpt-3.5-turbo" | "gpt3.5turbo" => Ok(Self::GPT35TURBO),
-            _ => Err(s.to_string())
+            _ => Err(s.to_string()),
         }
     }
 }
 
 // USD per 1M tokens
 // From https://openai.com/api/pricing/
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Copy, Debug, Clone, Serialize, Deserialize)]
 pub struct PricingInfo {
     pub input_tokens: f64,
     pub output_tokens: f64,
@@ -53,38 +55,39 @@ impl OpenAIModel {
             Self::GPT4O => PricingInfo {
                 input_tokens: 2.5,
                 output_tokens: 10.00,
-                cached_input_tokens: Some(1.25)
+                cached_input_tokens: Some(1.25),
             },
             Self::GPT4OMINI => PricingInfo {
                 input_tokens: 0.15,
                 cached_input_tokens: Some(0.075),
-                output_tokens: 0.6
+                output_tokens: 0.6,
             },
             Self::O1 => PricingInfo {
                 input_tokens: 15.00,
                 cached_input_tokens: Some(7.5),
-                output_tokens: 60.00
+                output_tokens: 60.00,
             },
             Self::O1MINI => PricingInfo {
                 input_tokens: 3.0,
                 cached_input_tokens: Some(1.5),
-                output_tokens: 12.00
+                output_tokens: 12.00,
             },
             Self::GPT35TURBO => PricingInfo {
                 input_tokens: 3.0,
                 cached_input_tokens: None,
-                output_tokens: 6.0
+                output_tokens: 6.0,
             },
             Self::GPT4 => PricingInfo {
                 input_tokens: 30.0,
                 output_tokens: 60.0,
-                cached_input_tokens: None
+                cached_input_tokens: None,
             },
             Self::GPT4TURBO => PricingInfo {
                 input_tokens: 10.0,
                 output_tokens: 30.0,
-                cached_input_tokens: None
-            }
+                cached_input_tokens: None,
+            },
+            Self::Other(_, pricing) => *pricing,
         }
     }
 
@@ -93,14 +96,14 @@ impl OpenAIModel {
             Self::GPT4O => Some(PricingInfo {
                 input_tokens: 1.25,
                 output_tokens: 5.00,
-                cached_input_tokens: None
+                cached_input_tokens: None,
             }),
             Self::GPT4OMINI => Some(PricingInfo {
                 input_tokens: 0.075,
                 output_tokens: 0.3,
-                cached_input_tokens: None
+                cached_input_tokens: None,
             }),
-            _ => None
+            _ => None,
         }
     }
 }
